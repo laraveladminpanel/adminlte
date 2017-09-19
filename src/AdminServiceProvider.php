36daf9d75545b42d69db82209e20dc131f0b638e
aleksey.kostenko.dev <aleksey.kostenko.dev@gmail.com>
2017-09-19 23:24:56 +0300

    added AdminServiceProvider

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LaravelAdminPanel\Admin;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @param \Illuminate\Routing\Router $router
+     */
+    public function boot(Router $router, Dispatcher $event)
+    {
+
+    }
+}
